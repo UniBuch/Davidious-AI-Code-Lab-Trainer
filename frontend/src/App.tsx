4e7cc746d05711c198102bc99dd477d7db640bf5
aylarsopyyeva173 <aylarsopyyeva173@gmail.com>
2026-05-02 18:47:05 +0300

    Implementing the design for all pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,11 @@ import Layout from './components/Layout';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
 import { Home } from './pages/Home';
+import DashboardPage from "./pages/Dashboard";
+import PathListPage from "./pages/PathList";
+import PathDetailPage from "./pages/PathDetail";
+import UploadPage from "./pages/Upload";
+import CreatePathPage from "./pages/CreatePath";
 
 function App() {
   return (
@@ -23,6 +28,11 @@ function App() {
 
               <Route element={<ProtectedRoute />}>
                 <Route path="/" element={<Home />} />
+                <Route path="/dashboard" element={<DashboardPage />} />
+                <Route path="/paths" element={<PathListPage />} />
+                <Route path="/paths/new" element={<CreatePathPage />} />
+                <Route path="/paths/:id" element={<PathDetailPage />} />
+                <Route path="/upload" element={<UploadPage />} />
               </Route>
 
             </Route>
